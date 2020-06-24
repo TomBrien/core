065d2f163b4775fbd2a70682d8f440446acf71d4
Tom Brien <tom@byth.co.uk>
2020-06-24 22:40:22 +0100

    get items

--- homeassistant/components/mobile_app/webhook.py
+++ homeassistant/components/mobile_app/webhook.py
@@ -338,7 +338,7 @@ async def webhook_update_registration(hass, config_entry, data):
 
     _LOGGER.warning(
         "HASS expects this to belong to "
-        + f"{registrations[new_registration[CONF_WEBHOOK_ID]]}"
+        + f"{registrations[new_registration[CONF_WEBHOOK_ID]].items()}"
     )
 
     device_registry = await dr.async_get_registry(hass)
